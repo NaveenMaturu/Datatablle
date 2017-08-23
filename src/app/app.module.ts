1d84257262695ca4d76267e331aea74c2ae9ef31
naveen <naveenmaturu@gmail.com>
2017-08-23 15:30:06 +0530

    datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MdTableModule} from '@angular/material';
+import {HttpModule} from '@angular/http';
+import{DataSource,CdkTableModule} from '@angular/cdk';
+/* import {CdkTableModule} from '@angular/cdk'; */
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+ imports: [
+    BrowserModule,
+	MdTableModule,
+	CdkTableModule,
+
+	HttpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
